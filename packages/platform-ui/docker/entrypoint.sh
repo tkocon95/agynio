02c2677d7435f5fcacfa9065eb615d67a96a82ca
casey-brooks <casey.brooks@agyn.io>
2026-03-04 23:40:24 +0000

    refactor(platform-ui): remove nginx proxy templating (#1366)

--- packages/platform-ui/docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-
-: "${API_UPSTREAM:=http://localhost:3010}"
-export API_UPSTREAM
-
-envsubst '$API_UPSTREAM' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
